3a8e4c82880ef4d45af993b1c3a2e789b866d886
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2024-11-28 00:31:09 +0900

    [섹션 6] 자동 구성(Auto Configuration)

---
+++ memory-v2/src/main/java/memory/Memory.java
@@ -0,0 +1,27 @@
+package memory;
+
+public class Memory {
+    private long used;
+    private long max;
+
+    public Memory(long used, long max) {
+        this.used = used;
+        this.max = max;
+    }
+
+    public long getMax() {
+        return max;
+    }
+
+    public long getUsed() {
+        return used;
+    }
+
+    @Override
+    public String toString() {
+        return "Memory{" +
+                "max=" + max +
+                ", used=" + used +
+                '}';
+    }
+}
